16870ffde6893c20bdab0e6a060e3ed790899c76
Aubrey Beltran <35947246+acbee8@users.noreply.github.com>
2018-09-22 16:37:51 -0700

    Add files via upload

---
+++ tweepy_config.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+api_key = "k8kKxgCW4elM4XJ4hDQI276oc"
+api_secret = "e3kpNJrTGa5mN4J60pnAEIuoovI8sNhQgId1tbVcttiXsjEnD9"
+
+
+acc_token = "752214977700700160-F0rOWJTpCbwDXvD6OhhrIGCRJcBIy44"
+acc_token_secret = "6JgtiGjUbnvlX6Yazda96gjZBRlvKnBQ9LNkQP9pfCkt3"
+
